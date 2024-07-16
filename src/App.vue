b87015fea8598db89b8be16ab183a33dfe709798
DDD5677 <djumaqulovdonisher@mail.ru>
2024-07-16 19:50:21 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="section">
+		<div class="container">
+			<div class="inner">
+				<LeftSidebar />
+			</div>
+		</div>
+	</div>
+</template>
+<script setup lang="ts">
+import LeftSidebar from "./components/LeftSidebar.vue"
+</script>
+
+
+<style lang="scss" scoped>
+.section {
+	min-height: 100vh;
+	background-color: #1D1D1D;
+}
+</style>
---
+++ src/components/LeftSidebar.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="left-sidebar">
+		<div class="img-box">
+			<img src="@/assets/image/left-sidebar.png" alt="">
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
